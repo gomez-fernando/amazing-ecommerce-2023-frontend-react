192b5a21b996048e225eff4241d669c46a3142b7
gomez-fernando <ferdgomez@hotmail.es>
2023-02-05 10:48:19 +0100

    Add product details page

---
+++ src/screens/ProductScreen.jsx
@@ -0,0 +1,12 @@
+import { useParams } from "react-router-dom";
+
+const ProductScreen = () => {
+  const params = useParams();
+  const {slug} = params;
+
+  return (
+    <div>{slug}</div>
+  )
+}
+
+export default ProductScreen
