57c3491552915f227664601a2fda2a80179cb95a
gomez-fernando <ferdgomez@hotmail.es>
2023-02-07 20:52:44 +0000

    Add Store

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { Container, Navbar } from "react-bootstrap";
+import { useContext } from "react";
+import { Badge, Container, Nav, Navbar } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import { Store } from "./Store";
 
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
+
   return (
     <BrowserRouter>
       <div className="d-flex flex-column site-container">
@@ -14,6 +19,16 @@ function App() {
               <LinkContainer to="/">
                 <Navbar.Brand>faShop</Navbar.Brand>
               </LinkContainer>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link" >
+                  Cart
+                  {cart.cartItems.length > 0 &&
+                    <Badge pill bg="danger">
+                      {cart.cartItems.length}
+                    </Badge>
+                  }
+                </Link>
+              </Nav>
             </Container>
           </Navbar>
         </header>
